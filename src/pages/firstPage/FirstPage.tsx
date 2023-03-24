a277e18196809cece7e08260c6333b5a6db5f568
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-03-24 23:53:19 +0600

    play with clip-path little bit

--- src/pages/firstPage/FirstPage.tsx
+++ src/pages/firstPage/FirstPage.tsx
@@ -1,8 +1,12 @@
 import "./FirstPage.css"
+import logo from "../../assets/logo-white.png";
 export default function FirstPage() {
   return (
     <div className="first-header">
-
+      {/* logo */}
+      <div className="logo-box">
+        <img className="logo" src={logo} alt="" />
+      </div>
     </div>
   )
 }
