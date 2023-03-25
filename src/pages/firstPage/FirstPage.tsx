e2d6b4076cb3a33a3aae893736fccf1ebd0c666f
Azharul-Raj <azharulislam.raj512@gmail.com>
2023-03-25 22:25:49 +0600

    centered element using absolute position

--- src/pages/firstPage/FirstPage.tsx
+++ src/pages/firstPage/FirstPage.tsx
@@ -2,10 +2,20 @@ import "./FirstPage.css"
 import logo from "../../assets/logo-white.png";
 export default function FirstPage() {
   return (
-    <div className="first-header">
-      {/* logo */}
-      <div className="logo-box">
-        <img className="logo" src={logo} alt="" />
+    <div className="">
+      <div className="first-header">
+        {/* logo */}
+        <div className="logo-box">
+          <img className="logo" src={logo} alt="" />
+        </div>
+
+        {/* main body */}
+        <div className="main-box">
+          <div className="text-box">
+            <span className="main-heading">OUTDOORS</span>
+            <span className="sub-heading">is where life happens</span>
+          </div>
+        </div>
       </div>
     </div>
   )
